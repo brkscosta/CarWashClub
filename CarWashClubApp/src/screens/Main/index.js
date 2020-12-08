a1e798f81738e909a6a01a4d9e8e126d02131d93
brkscosta <joanacosta97@hotmail.com>
2020-12-08 06:43:31 +0000

    Hide .env

--- CarWashClubApp/src/screens/Main/index.js
+++ CarWashClubApp/src/screens/Main/index.js
@@ -2,6 +2,8 @@ import React from 'react';
 import { View, Text, Button, StatusBar } from 'react-native';
 import { useAuth } from '../../contexts/auth';
 
+//TODO Next part of the project, build the main screen
+
 const MainScreeen = () => {
   const { user, signOut } = useAuth();
 
